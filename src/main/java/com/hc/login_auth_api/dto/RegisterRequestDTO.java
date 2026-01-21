266afc4d8660ecb9c2351accfe631dc620ff7dea
EuHttl <hyttalo2002@gmail.com>
2026-01-21 08:59:22 -0300

    feat: Add models DTO and Authentication.

---
+++ src/main/java/com/hc/login_auth_api/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.hc.login_auth_api.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+}
---
+++ src/main/java/com/hc/login_auth_api/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.hc.login_auth_api.dto;
+
+public record ResponseDTO(String name, String token) {
+}
