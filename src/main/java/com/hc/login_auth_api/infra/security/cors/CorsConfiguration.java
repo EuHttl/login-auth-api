fd6856515cb4a1f0a0e110cd040598df1ffc09f6
EuHttl <hyttalo2002@gmail.com>
2026-01-21 09:01:50 -0300

    feat: implements auth JWT and filters security.

---
+++ src/main/java/com/hc/login_auth_api/infra/security/cors/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.hc.login_auth_api.infra.security.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration {
+    @Configuration
+    public class CorsConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "DELETE", "PUT");
+        }
+    }
+}
